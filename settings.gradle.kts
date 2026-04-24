e4969e80393c506964213ef45b0ba803e45e1739
Bruno Vargas <brunodev.pdl@gmail.com>
2026-04-24 07:23:31 -0300

    chore: bootstrap compose multiplatform base configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "recipe-cost-calculator"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":composeApp")
+include(":shared")
